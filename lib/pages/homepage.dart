b1ac0ee868c0da63d97516a062d5d998d810afd1
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-10 15:08:01 -0300

    commit 6

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,9 @@
+import 'package:app_livraria_flutter/models/livro.dart';
 import 'package:app_livraria_flutter/pages/novo_livro.dart';
 import 'package:app_livraria_flutter/themes/themes_colors.dart';
 import 'package:flutter/material.dart';
+
+import '../services/livro_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,35 +13,60 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  LivroService service = LivroService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Lista de livros"),
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return Column(
-                  children: [
-                    ListTile(
-                      title: const Text("Titulo"),
-                      subtitle: const Text("Autor"),
-                      onTap: () {},
+      body: StreamBuilder(
+        stream: Stream.fromFuture(service.getAll()),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return SingleChildScrollView(
+              child: Center(
+                child: Text("Erro: ${snapshot.error}"),
+              ),
+            );
+          } else {
+            List<Livro>? lista = snapshot.data;
+
+            if (lista != null && lista.isNotEmpty) {
+              return CustomScrollView(
+                slivers: [
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        return Column(
+                          children: [
+                            ListTile(
+                              title: Text(lista[index].titulo),
+                              subtitle: Text(lista[index].autor),
+                              onTap: () {},
+                            ),
+                            const Divider(),
+                          ],
+                        );
+                      },
+                      childCount: lista.length,
                     ),
-                    const Divider(),
-                  ],
-                );
-              },
-              childCount: 10,
-            ),
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(height: 80),
-          ),
-        ],
+                  ),
+                  const SliverToBoxAdapter(
+                    child: SizedBox(height: 80),
+                  ),
+                ],
+              );
+            } else {
+              return const Center(
+                child: Text("Lista vazia"),
+              );
+            }
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
